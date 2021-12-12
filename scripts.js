877537fe37d8106a15d587520cc607cc19e59265
Lyonsmasawa <lyonsmasawa@gmail.com>
2021-12-12 11:38:33 -0800

    store user input

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
 //arrays
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena","Kwaku","Yaw","Kofi", "Kwame"];
 let femaleNames = ["Akosua", "Adwoa", "Abenaa","Akua","Yaa","Afua", "Ama"];
-let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+//get input values
+let yearOfBirth = document.getElementById("year").value;
+let month = document.getElementById("month").value;
+let day = document.getElementById("day").value;
+let genderValue = document.getElementsByName("gender").value;
