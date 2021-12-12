ca939600d723132274512896db05eb2d0fe2020c
Lyonsmasawa <lyonsmasawa@gmail.com>
2021-12-12 11:54:31 -0800

    get gender function

--- scripts.js
+++ scripts.js
@@ -1,10 +1,19 @@
 //arrays
-let maleNames = ["Kwasi", "Kwadwo", "Kwabena","Kwaku","Yaw","Kofi", "Kwame"];
-let femaleNames = ["Akosua", "Adwoa", "Abenaa","Akua","Yaa","Afua", "Ama"];
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 //get input values
 let yearOfBirth = document.getElementById("year").value;
 let month = document.getElementById("month").value;
 let day = document.getElementById("day").value;
-let genderValue = document.getElementsByName("gender").value;
+
+//get gender value
+function getGender {
+    let genderValue = document.getElementsByName("gender");
+    if (genderValue.checked[0]) {
+        let gender = "male";
+    } else {
+        let gender = "female";
+    }
+}
