c1add6d33e6a3a0564cb66445a68825db9d4776c
Lyonsmasawa <lyonsmasawa@gmail.com>
2021-12-12 10:58:42 -0800

    add days

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
 //arrays
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena","Kwaku","Yaw","Kofi", "Kwame"];
 let femaleNames = ["Kwasi", "Kwadwo", "Kwabena","Kwaku","Yaw","Kofi", "Kwame"];
+let day = ["Sunday", "Monday", "Tuesday", "Wednesday", ""]
