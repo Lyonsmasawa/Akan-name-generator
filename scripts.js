6b0785807f9f27e24401541c4f5293a3894a8026
Lyonsmasawa <lyonsmasawa@gmail.com>
2021-12-12 10:47:56 -0800

    arrays with respective names

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
 //arrays
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena","Kwaku","Yaw","Kofi", "Kwame"];
-let femaleNames = ["Kwasi", "Kwadwo", "Kwabena","Kwaku","Yaw","Kofi", "Kwame"]
+let femaleNames = ["Kwasi", "Kwadwo", "Kwabena","Kwaku","Yaw","Kofi", "Kwame"];
